3586794fe0a70828f695d4e0dac371990e87a5cb
SajuJ <162431432+SajuJ@users.noreply.github.com>
2024-07-20 17:59:09 +0100

    added /health CDP api to stop 404 notifications on OS dashboard

--- src/api/health/controller.js
+++ src/api/health/controller.js
@@ -1,5 +1,5 @@
 const healthController = {
-    handler: (request, h) => h.response({ message: 'success' }).code(200)
-  }
-  
-  export { healthController }
+  handler: (request, h) => h.response({ message: 'success' }).code(200)
+}
+
+export { healthController }
