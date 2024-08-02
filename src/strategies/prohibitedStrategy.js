eacc3f4df488f7abe4b5e906d29510c5f6404b01
Abirami <abirami.sakthivel@cognizant.com>
2024-08-02 14:13:57 +0100

    sorting remove from map function

--- src/strategies/prohibitedStrategy.js
+++ src/strategies/prohibitedStrategy.js
@@ -1126,83 +1126,86 @@ class ProhibitedStrategy extends workflowEngine {
       let a11RulesFetched = false
 
       if (Array.isArray(plantDocument.HOST_REGULATION.ANNEX11)) {
-        const promises = plantDocument.HOST_REGULATION.ANNEX11.sort(
-          sortAnnex11
-        ).map(async (annex11) => {
-          // SPECIES
-          annex11CountrySpecies =
-            await getAnnex11ForHRCountrySvcFmtSpecies(annex11)
-          if (
-            typeof annex11CountrySpecies === 'object' &&
-            annex11CountrySpecies !== null
-          ) {
-            annex11CountrySpeciesArr.push(annex11CountrySpecies)
-          }
+        const promises = plantDocument.HOST_REGULATION.ANNEX11.map(
+          async (annex11) => {
+            // SPECIES
+            annex11CountrySpecies =
+              await getAnnex11ForHRCountrySvcFmtSpecies(annex11)
+            if (
+              typeof annex11CountrySpecies === 'object' &&
+              annex11CountrySpecies !== null
+            ) {
+              annex11CountrySpeciesArr.push(annex11CountrySpecies)
+            }
 
-          annex11RegionSpecies =
-            await getAnnex11ForHRRegionSvcFmtSpecies(annex11)
-          if (
-            typeof annex11RegionSpecies === 'object' &&
-            annex11RegionSpecies !== null
-          ) {
-            annex11RegionSpeciesArr.push(annex11RegionSpecies)
-          }
+            annex11RegionSpecies =
+              await getAnnex11ForHRRegionSvcFmtSpecies(annex11)
+            if (
+              typeof annex11RegionSpecies === 'object' &&
+              annex11RegionSpecies !== null
+            ) {
+              annex11RegionSpeciesArr.push(annex11RegionSpecies)
+            }
 
-          annex11AllSpecies = await getAnnex11ForHRAllSvcFmtFamily(annex11)
-          if (
-            typeof annex11AllSpecies === 'object' &&
-            annex11AllSpecies !== null
-          ) {
-            annex11AllSpeciesArr.push(annex11AllSpecies)
-          }
+            annex11AllSpecies = await getAnnex11ForHRAllSvcFmtFamily(annex11)
+            if (
+              typeof annex11AllSpecies === 'object' &&
+              annex11AllSpecies !== null
+            ) {
+              annex11AllSpeciesArr.push(annex11AllSpecies)
+            }
 
-          // GENUS
-          annex11CountryGenus = await getAnnex11ForCountrySvcFmtGenus(annex11)
-          if (
-            typeof annex11CountryGenus === 'object' &&
-            annex11CountryGenus !== null
-          ) {
-            annex11CountryGenusArr.push(annex11CountryGenus)
-          }
+            // GENUS
+            annex11CountryGenus = await getAnnex11ForCountrySvcFmtGenus(annex11)
+            if (
+              typeof annex11CountryGenus === 'object' &&
+              annex11CountryGenus !== null
+            ) {
+              annex11CountryGenusArr.push(annex11CountryGenus)
+            }
 
-          annex11RegionGenus = await getAnnex11ForRegionSvcFmtGenus(annex11)
-          if (
-            typeof annex11RegionGenus === 'object' &&
-            annex11RegionGenus !== null
-          ) {
-            annex11RegionGenusArr.push(annex11RegionGenus)
-          }
+            annex11RegionGenus = await getAnnex11ForRegionSvcFmtGenus(annex11)
+            if (
+              typeof annex11RegionGenus === 'object' &&
+              annex11RegionGenus !== null
+            ) {
+              annex11RegionGenusArr.push(annex11RegionGenus)
+            }
 
-          annex11AllGenus = await getAnnex11ForAllSvcFmtGenus(annex11)
-          if (typeof annex11AllGenus === 'object' && annex11AllGenus !== null) {
-            annex11AllGenusArr.push(annex11AllGenus)
-          }
+            annex11AllGenus = await getAnnex11ForAllSvcFmtGenus(annex11)
+            if (
+              typeof annex11AllGenus === 'object' &&
+              annex11AllGenus !== null
+            ) {
+              annex11AllGenusArr.push(annex11AllGenus)
+            }
 
-          // ALL
-          annex11CountryFamily = await getAnnex11ForAllSvcFmtCountry(annex11)
-          if (
-            typeof annex11CountryFamily === 'object' &&
-            annex11CountryFamily !== null
-          ) {
-            annex11CountryFamilyArr.push(annex11CountryFamily)
-          }
+            // ALL
+            annex11CountryFamily = await getAnnex11ForAllSvcFmtCountry(annex11)
+            if (
+              typeof annex11CountryFamily === 'object' &&
+              annex11CountryFamily !== null
+            ) {
+              annex11CountryFamilyArr.push(annex11CountryFamily)
+            }
 
-          annex11RegionFamily = await getAnnex11ForAllSvcFmtRegion(annex11)
-          if (
-            typeof annex11RegionFamily === 'object' &&
-            annex11RegionFamily !== null
-          ) {
-            annex11RegionFamilyArr.push(annex11RegionFamily)
-          }
+            annex11RegionFamily = await getAnnex11ForAllSvcFmtRegion(annex11)
+            if (
+              typeof annex11RegionFamily === 'object' &&
+              annex11RegionFamily !== null
+            ) {
+              annex11RegionFamilyArr.push(annex11RegionFamily)
+            }
 
-          annex11AllFamily = await getAnnex11ForAllSvcFmt(annex11)
-          if (
-            typeof annex11AllFamily === 'object' &&
-            annex11AllFamily !== null
-          ) {
-            annex11AllFamilyArr.push(annex11AllFamily)
+            annex11AllFamily = await getAnnex11ForAllSvcFmt(annex11)
+            if (
+              typeof annex11AllFamily === 'object' &&
+              annex11AllFamily !== null
+            ) {
+              annex11AllFamilyArr.push(annex11AllFamily)
+            }
           }
-        })
+        )
         await Promise.all(promises)
       }
 
@@ -1214,7 +1217,7 @@ class ProhibitedStrategy extends workflowEngine {
         plantInfo.outcome.toLowerCase() !== 'prohibited'
       ) {
         logger.info('annex11CountrySpeciesArr.length > 0')
-        plantInfo.annex11RulesArr = annex11CountrySpeciesArr
+        plantInfo.annex11RulesArr = annex11CountrySpeciesArr?.sort(sortAnnex11)
         a11RulesFetched = true
       }
 
@@ -1225,7 +1228,7 @@ class ProhibitedStrategy extends workflowEngine {
         plantInfo.outcome.toLowerCase() !== 'prohibited'
       ) {
         logger.info('annex11RegionSpeciesArr.length > 0')
-        plantInfo.annex11RulesArr = annex11RegionSpeciesArr
+        plantInfo.annex11RulesArr = annex11RegionSpeciesArr?.sort(sortAnnex11)
         a11RulesFetched = true
       }
 
@@ -1236,7 +1239,7 @@ class ProhibitedStrategy extends workflowEngine {
         plantInfo.outcome.toLowerCase() !== 'prohibited'
       ) {
         logger.info('annex11AllSpeciesArr.length > 0')
-        plantInfo.annex11RulesArr = annex11AllSpeciesArr
+        plantInfo.annex11RulesArr = annex11AllSpeciesArr?.sort(sortAnnex11)
         a11RulesFetched = true
       }
 
@@ -1248,7 +1251,7 @@ class ProhibitedStrategy extends workflowEngine {
         plantInfo.outcome.toLowerCase() !== 'prohibited'
       ) {
         logger.info('annex11CountryGenusArr.length > 0')
-        plantInfo.annex11RulesArr = annex11CountryGenusArr
+        plantInfo.annex11RulesArr = annex11CountryGenusArr?.sort(sortAnnex11)
         a11RulesFetched = true
       }
 
@@ -1259,7 +1262,7 @@ class ProhibitedStrategy extends workflowEngine {
         plantInfo.outcome.toLowerCase() !== 'prohibited'
       ) {
         logger.info('annex11RegionGenusArr.length > 0')
-        plantInfo.annex11RulesArr = annex11RegionGenusArr
+        plantInfo.annex11RulesArr = annex11RegionGenusArr?.sort(sortAnnex11)
         a11RulesFetched = true
       }
 
@@ -1270,7 +1273,7 @@ class ProhibitedStrategy extends workflowEngine {
         plantInfo.outcome.toLowerCase() !== 'prohibited'
       ) {
         logger.info('annex11AllGenusArr.length > 0')
-        plantInfo.annex11RulesArr = annex11AllGenusArr
+        plantInfo.annex11RulesArr = annex11AllGenusArr?.sort(sortAnnex11)
         a11RulesFetched = true
       }
 
@@ -1282,7 +1285,7 @@ class ProhibitedStrategy extends workflowEngine {
         plantInfo.outcome.toLowerCase() !== 'prohibited'
       ) {
         logger.info('annex11CountryFamilyArr.length > 0')
-        plantInfo.annex11RulesArr = annex11CountryFamilyArr
+        plantInfo.annex11RulesArr = annex11CountryFamilyArr?.sort(sortAnnex11)
         a11RulesFetched = true
       }
 
@@ -1293,7 +1296,7 @@ class ProhibitedStrategy extends workflowEngine {
         plantInfo.outcome.toLowerCase() !== 'prohibited'
       ) {
         logger.info('annex11RegionFamilyArr.length > 0')
-        plantInfo.annex11RulesArr = annex11RegionFamilyArr
+        plantInfo.annex11RulesArr = annex11RegionFamilyArr?.sort(sortAnnex11)
         a11RulesFetched = true
       }
 
@@ -1304,7 +1307,7 @@ class ProhibitedStrategy extends workflowEngine {
         plantInfo.outcome.toLowerCase() !== 'prohibited'
       ) {
         logger.info('annex11AllFamilyArr.length > 0')
-        plantInfo.annex11RulesArr = annex11AllFamilyArr
+        plantInfo.annex11RulesArr = annex11AllFamilyArr?.sort(sortAnnex11)
         a11RulesFetched = true
       }
 
